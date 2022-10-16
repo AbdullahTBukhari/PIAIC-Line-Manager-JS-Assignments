05a1448f4ca050b9566ae43558a3f64a76968e29
Abdullah T Bukhari <m.abdullahtbukhari@gmail.com>
2022-10-16 05:56:36 +0500

    Q: 04 Done

---
+++ Q-04-Famous-Quote.js
@@ -0,0 +1,3 @@
+let _name = 'Ali Jinnah';
+let _quote = 'Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.';
+console.log(`${_name} once said, ${_quote}`);
