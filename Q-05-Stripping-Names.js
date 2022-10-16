c96f0f2f32441c3d6cedb406b678d750fa9112e4
Abdullah T Bukhari <m.abdullahtbukhari@gmail.com>
2022-10-16 07:09:15 +0500

    Q: 05 Done using Trim method

---
+++ Q-05-Stripping-Names.js
@@ -0,0 +1,5 @@
+let _name = ' Umar ';
+// log with spaces
+console.log(`${_name} is log with spaces`);
+// log with removal of start and end white spaces in name
+console.log(`${_name.trim()} is log with removal of start and end white spaces in name.`);
