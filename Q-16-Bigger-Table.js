0e09866c26a36e345236767f49875090917a0d5b
Abdullah T Bukhari <m.abdullahtbukhari@gmail.com>
2022-10-18 04:47:19 +0500

    Create Q-16-Bigger-Table.js
    added into the list from start, middle and the end then logged.

---
+++ Q-16-Bigger-Table.js
@@ -0,0 +1,20 @@
+let guestList = [
+    { name: 'Ali Jinnah'},
+    { name: 'Allama Iqbal'},
+    { name: 'Tesla'}
+];
+let newGuest = 'Dr. Israr Ahmad';
+let unAvailableGuest = 'Tesla';
+let replaceGuest = (oldGuest, newGuest, list) => {
+    list.forEach((item) => {
+        if (item.name==oldGuest) {
+            item.name = newGuest;
+        }
+    })
+}
+// replaceGuest(unAvailableGuest,  newGuest, guestList);
+guestList.unshift({name: 'Tesla'});
+guestList.splice(2, 0, {name: 'Nelson Mandella'});
+guestList.push({name: 'Dr AbdulQADEER Khan'});
+guestList.forEach(guest=>console.log(`Greetings! Welcome Sir ${guest.name}`));
+console.log(`We have got a bigger table...!! 😊`);
