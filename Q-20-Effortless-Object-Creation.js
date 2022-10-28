4dbc75d550837a333e73a6bfecdbbaaf6309a4a9
Abdullah T Bukhari <m.abdullahtbukhari@gmail.com>
2022-10-28 16:45:25 +0500

    efforless object creation

---
+++ Q-20-Effortless-Object-Creation.js
@@ -0,0 +1,10 @@
+let arr=[];
+function listCreate(items) {
+    arr.push(items);
+    return arr;
+}
+
+console.log(listCreate("K-2"));
+console.log(listCreate("Everest"));
+console.log(listCreate("Murree"));
+
